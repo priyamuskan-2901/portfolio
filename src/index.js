3c37f5b735ea5821a7c015f6632b412e6586dd0d
priyamuskan-2901 <muskan.priya.civ21@itbhu.ac.in>
2024-10-11 17:54:23 +0530

    dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Change here
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+// Create a root for the React application
+const root = ReactDOM.createRoot(document.getElementById("root")); // Change here
+
+// Render the App component inside the root
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
